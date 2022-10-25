13667c2a30a1dadccd2981f1bae65a3382b27fce
Vlad-Andrei Gruia <58209761+vladandrei228@users.noreply.github.com>
2022-10-26 00:01:24 +0200

    Add files via upload

---
+++ build/precache-manifest.bb66045e39ef1c04f39c05cf41296d5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4191e02f7a07ac754b73",
+    "url": "/static/css/main.86ccb68f.chunk.css"
+  },
+  {
+    "revision": "4191e02f7a07ac754b73",
+    "url": "/static/js/main.4191e02f.chunk.js"
+  },
+  {
+    "revision": "ec9af79bffa5c3f8e9f8",
+    "url": "/static/js/1.ec9af79b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af747db8a7c55ff78625faf4df15f3bb",
+    "url": "/index.html"
+  }
+];
